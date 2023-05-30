025f830aba66bf328636ce550c0399c24afe4bbd
HookSandeer <hooksander@proton.me>
2023-05-30 08:33:21 +0200

    Update Script

--- fix.py
+++ fix.py
@@ -2,7 +2,7 @@
 # By HookSander
 import os
 os.system("synclient tapbutton1=1")
-os.system("synclient tabbutton2=3")
+os.system("synclient tapbutton2=3")
 os.system("syndaemon -i 1 -K -t -d")
 print("Enter to exit")
 tmp = input()
