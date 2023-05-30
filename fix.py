b668dba88fe5c7e54cf71760bf919b3a21f69154
HookSandeer <hooksander@proton.me>
2023-05-30 08:28:27 +0200

    Add script

---
+++ fix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# By HookSander
+import os
+os.system("synclient tapbutton1=1")
+os.system("synclient tabbutton2=3")
+os.system("syndaemon -i 1 -K -t -d")
+print("Enter to exit")
+tmp = input()
+exit()
